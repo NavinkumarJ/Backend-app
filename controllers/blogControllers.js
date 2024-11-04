a87f0e186d5e7f1fb8b4861305170859a010bd95
NavinkumarJ <navinkumar02032005@gmail.com>
2024-11-04 14:32:37 +0530

    third-commit

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,7 +1,11 @@
 function getAllBlogPost(req,res) {
     res.send("Get Route");
 }
+function getSpecific(req,res) {
+    res.send("Specific route");
+}
 
 module.exports = {
     getAllBlogPost,
+    getSpecific,
 }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 const BlogControllers = require("../controllers/blogControllers");
 router.get("/getAllBlogPost", BlogControllers.getAllBlogPost);
+router.get("/getSpecific", BlogControllers.getSpecific);
 
 module.exports = router;
